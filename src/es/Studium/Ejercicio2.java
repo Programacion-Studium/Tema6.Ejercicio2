952616bfdb967b7486c4d01e47fa83c66f92c552
Alvca <Alvca@DESKTOP-H3LD864>
2020-02-06 08:37:38 +0100

    1

---
+++ src/es/Studium/Ejercicio2.java
@@ -0,0 +1,29 @@
+package es.Studium;
+import java.util.*;
+public class Ejercicio2
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			int numero1, numero2;
+			Scanner teclado = new Scanner(System.in);
+			System.out.println("Escriba un número entero: ");
+			numero1=teclado.nextInt();
+			System.out.println("Escriba otro número entero: ");
+			numero2=teclado.nextInt();
+			teclado.close();
+			double resultado;
+			resultado=(double)numero1/(double)numero2;
+			System.out.println(resultado);
+		}
+		catch(ArithmeticException err)
+		{
+			System.out.println("El divisor no puede ser 0");
+		}catch(InputMismatchException err)
+		{
+			System.out.println("No se ha introducido un número entero...");
+		}
+		
+	}
+}
